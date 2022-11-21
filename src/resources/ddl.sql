8bd5fc8c560d22721f713ce97340d4bdd6b8591a
Serge Rozkov <rozkov_ss@mail.ru>
2022-11-21 22:05:38 +0300

    add ddl

---
+++ src/resources/ddl.sql
@@ -0,0 +1,50 @@
+-- CREATE DATABASE project;
+CREATE SCHEMA IF NOT EXISTS faculty;
+SET search_path TO faculty;
+
+CREATE TABLE IF NOT EXISTS faculty.course(
+                                             id BIGSERIAL PRIMARY KEY,
+                                             course_name VARCHAR(255)
+    );
+
+CREATE TABLE IF NOT EXISTS faculty.address (
+                                              id BIGSERIAL PRIMARY KEY,
+                                              city VARCHAR(64),
+                                              street VARCHAR(64),
+                                              house VARCHAR(64),
+                                              apartment VARCHAR(64)
+    );
+
+CREATE TABLE IF NOT EXISTS faculty.teacher(
+                                              id BIGSERIAL PRIMARY KEY,
+                                              first_name VARCHAR(128),
+                                              last_name VARCHAR(128),
+                                              address BIGSERIAL REFERENCES address(id),
+                                              course BIGSERIAL REFERENCES course(id)
+    );
+
+CREATE TABLE IF NOT EXISTS faculty.student(
+                                              id BIGSERIAL PRIMARY KEY,
+                                              first_name VARCHAR(128) NOT NULL ,
+                                              last_name VARCHAR(128) NOT NULL
+    );
+
+
+
+CREATE TABLE IF NOT EXISTS archive(
+                                              id BIGSERIAL PRIMARY KEY,
+                                              student_id BIGSERIAL REFERENCES faculty.student(id),
+                                              course_id BIGSERIAL REFERENCES faculty.course(id),
+                                              mark INT,
+                                              date_of_exam DATE
+    );
+
+CREATE TABLE IF NOT EXISTS faculty.students_courses(
+                                                       student_id BIGSERIAL REFERENCES faculty.student(id),
+                                                       course_id BIGSERIAL REFERENCES faculty.course(id)
+    );
+
+CREATE TABLE IF NOT EXISTS faculty.students_teachers(
+                                                       student_id BIGSERIAL REFERENCES faculty.student(id),
+                                                       teacher_id BIGSERIAL REFERENCES faculty.teacher(id)
+    );
