65d48dc16651e539b6956248e85f65a069be8da3
Serge Rozkov <rozkov_ss@mail.ru>
2022-11-21 23:58:30 +0300

    fix mentions

--- src/resources/ddl.sql
+++ src/resources/ddl.sql
@@ -2,41 +2,44 @@
 CREATE SCHEMA IF NOT EXISTS faculty;
 SET search_path TO faculty;
 
-CREATE TABLE IF NOT EXISTS faculty.course(
-                                             id BIGSERIAL PRIMARY KEY,
-                                             course_name VARCHAR(255)
+CREATE TABLE IF NOT EXISTS course(
+                                  id BIGSERIAL PRIMARY KEY,
+                                  course_name VARCHAR(255),
+                                  course_description VARCHAR(255)
     );
 
-CREATE TABLE IF NOT EXISTS faculty.address (
-                                              id BIGSERIAL PRIMARY KEY,
-                                              city VARCHAR(64),
-                                              street VARCHAR(64),
-                                              house VARCHAR(64),
-                                              apartment VARCHAR(64)
+CREATE TABLE IF NOT EXISTS address (
+                                    id BIGSERIAL PRIMARY KEY,
+                                    city VARCHAR(64),
+                                    street VARCHAR(64),
+                                    house VARCHAR(64),
+                                    apartment VARCHAR(64)
     );
 
-CREATE TABLE IF NOT EXISTS faculty.teacher(
-                                              id BIGSERIAL PRIMARY KEY,
-                                              first_name VARCHAR(128),
-                                              last_name VARCHAR(128),
-                                              address BIGSERIAL REFERENCES address(id),
-                                              course BIGSERIAL REFERENCES course(id)
+CREATE TABLE IF NOT EXISTS teacher(
+                                   id BIGSERIAL PRIMARY KEY,
+                                   first_name VARCHAR(128),
+                                   last_name VARCHAR(128),
+                                   email VARCHAR(64) UNIQUE
+                                   address BIGSERIAL REFERENCES address(id),
+                                   course BIGSERIAL REFERENCES course(id)
     );
 
-CREATE TABLE IF NOT EXISTS faculty.student(
-                                              id BIGSERIAL PRIMARY KEY,
-                                              first_name VARCHAR(128) NOT NULL ,
-                                              last_name VARCHAR(128) NOT NULL
+CREATE TABLE IF NOT EXISTS student(
+                                   id BIGSERIAL PRIMARY KEY,
+                                   first_name VARCHAR(128) NOT NULL ,
+                                   last_name VARCHAR(128) NOT NULL,
+                                   email VARCHAR(64) UNIQUE
     );
 
 
 
 CREATE TABLE IF NOT EXISTS archive(
-                                              id BIGSERIAL PRIMARY KEY,
-                                              student_id BIGSERIAL REFERENCES faculty.student(id),
-                                              course_id BIGSERIAL REFERENCES faculty.course(id),
-                                              mark INT,
-                                              date_of_exam DATE
+                                   id BIGSERIAL PRIMARY KEY,
+                                   student_id BIGSERIAL REFERENCES faculty.student(id),
+                                   course_id BIGSERIAL REFERENCES faculty.course(id),
+                                   mark INT,
+                                   date_of_exam DATE
     );
 
 CREATE TABLE IF NOT EXISTS faculty.students_courses(
